7b3297e13d04fc86031a2f3ecdd13006caae84be
Dan Koskei <qimtaid@gmail.com>
2024-03-21 13:35:01 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function receivesAFunction(callback) {
+   
+    callback();
+}
+
+
+function returnsANamedFunction() {
+ 
+    return function namedFunction() {
+        console.log("This is a named function");
+    };
+}
+
+
+function returnsAnAnonymousFunction() {
+   
+    return function() {
+        console.log("This is an anonymous function");
+    };
+}
